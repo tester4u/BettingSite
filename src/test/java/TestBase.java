838ce355e30d44b148d0611892ab904be14ef0be
rzak <rafal.zak@alan-systems.com>
2019-06-09 22:34:29 +0200

    comments

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,6 +1,8 @@
 
 //Not much comments, I try to write self-documenting code.
-//ChromeDriver 75.0.3770.8
+//Works on Linux and Windows
+//Linux: ChromeDriver 74.0.3729.6, Chromium Version 74.0.3729.169
+//Windows: ChromeDriver 75.0.3770.8, Chrome Version 75.0.3770.80
 //
 //Premier League season is over, so I couldn't bet on match event. Instead I've chosen to bet 'outright' on favourite.
 
